a7f608acd14fff4c2a5597178619983871f3bdb4
Ksars22 <kylesarsfield@icloud.com>
2022-09-18 23:15:04 -0400

    api key

--- src/apis/riotgames.js
+++ src/apis/riotgames.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const APIKEY = "RGAPI-82da79ac-948a-490f-936f-9da6bda20462";
+export const APIKEY = "RGAPI-27e8a814-8991-4c17-af9e-2373490f11cf";
 
 export const riotgames = axios.create({
   baseURL: "https://na1.api.riotgames.com/lol",
